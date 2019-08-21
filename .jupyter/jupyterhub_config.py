b9dc8f73396269d693d5aa45c13c4c9aa612c648
Alex Corvin <acorvin@redhat.com>
2019-08-21 08:31:42 -0400

    Support customization of Jupyterhub admin users
    This adds support for customizing the list of user IDs that should
    be granted admin access to JupyterHub via an environment variable, JUPYTERHUB_ADMIN_USERS

--- .jupyter/jupyterhub_config.py
+++ .jupyter/jupyterhub_config.py
@@ -309,3 +309,6 @@ c.KubeSpawner.pvc_name_template = '%s-nb-{username}-pvc' % os.environ['JUPYTERHU
 c.KubeSpawner.volumes = [dict(name='data', persistentVolumeClaim=dict(claimName=c.KubeSpawner.pvc_name_template))]
 c.KubeSpawner.volume_mounts = [dict(name='data', mountPath='/opt/app-root/src')]
 c.KubeSpawner.user_storage_class = os.environ.get("JUPYTERHUB_STORAGE_CLASS", c.KubeSpawner.user_storage_class)
+admin_users = os.environ.get('JUPYTERHUB_ADMIN_USERS')
+if admin_users:
+    c.Authenticator.admin_users = set(admin_users.split(','))
